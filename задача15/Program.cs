61ad1d9147ebf65b5e8ac3eb09b712b464c5ec1d
Anna <99716@mail.ru>
2023-06-28 01:07:58 +0300

    задача15

---
+++ задача15/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Введите число: ");
+int Digit = Convert.ToInt32(Console.ReadLine());
+int DayOff1 = 6;
+int DayOff2 = 7;
+while (Digit > 7)
+{
+    Console.WriteLine("Такого дня недели не существует");
+    break;
+}
+while (Digit < 6)
+{
+    Console.WriteLine("Рабочий день");
+    break;
+}
+while (Digit == DayOff1)
+{
+    Console.WriteLine("Выходной");
+    break;
+}
+while (Digit == DayOff2)
+{
+    Console.WriteLine("Выходной");
+    break;
+}
