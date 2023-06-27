304767ea8a4ef36ec3db4714bf94357fb25b34c7
Anna <99716@mail.ru>
2023-06-27 22:50:15 +0300

    задача10

---
+++ Задача10/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите первое число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+int secondDigit = Number / 10;
+int Digit = (secondDigit / 10) * 10;
+int result = secondDigit - Digit;
+Console.WriteLine($"В числе {Number} -> второе число {result}");
